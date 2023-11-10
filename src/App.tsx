ce9bbe59055fc6478aa0d8b9be54999a48307335
xgenio3 <xgenio3@gatech.edu>
2023-11-10 00:59:18 -0500

    made final-web (react/tsx)

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -5,9 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://mystickermania.com/cdn/stickers/memes/bugcat-capoo-im-shocked-512x512.png" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Xavier Genio
         </a>
       </header>
     </div>
